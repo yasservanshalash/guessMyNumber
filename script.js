70059fe38c08b97e7c7cab4605c191eeb0ee61ed
Yasser Shalash <yasservanshalash@gmail.com>
2022-11-06 15:51:16 +0100

    added loss feature

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ let score = document.querySelector(".score");
 let guess = document.querySelector(".guess");
 let check = document.querySelector(".check");
 let highscore = document.querySelector(".highscore");
+let body = document.querySelector("body");
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let scoreNumber = 20;
@@ -17,24 +18,33 @@ let highscoreNumber = 0;
 
 check.addEventListener("click", function () {
   const guessedNumber = Number(guess.value);
-
   if (!guessedNumber || guessedNumber <= 0 || guessedNumber > 20) {
     message.textContent = "🚫 Wrong Input";
   } else if (guessedNumber > secretNumber) {
-    message.textContent = "👇🏾 Lower!";
-    scoreNumber--;
-    score.textContent = scoreNumber;
+    if (scoreNumber <= 0) {
+      message.textContent = "💥 You Lost";
+      body.style.backgroundColor = "red";
+    } else {
+      message.textContent = "👇🏾 Lower!";
+      scoreNumber--;
+      score.textContent = scoreNumber;
+    }
   } else if (guessedNumber < secretNumber) {
-    message.textContent = "👆🏾 Higher!";
-    scoreNumber--;
-    score.textContent = scoreNumber;
+    if (scoreNumber <= 0) {
+      message.textContent = "💥 You Lost";
+      body.style.backgroundColor = "red";
+    } else {
+      message.textContent = "👆🏾 Higher!";
+      scoreNumber--;
+      score.textContent = scoreNumber;
+    }
   } else {
     message.textContent = "🎉 YOU WIN!";
     if (scoreNumber > highscoreNumber) {
       highscore.textContent = scoreNumber;
     }
     number.textContent = secretNumber;
-    document.querySelector("body").style.backgroundColor = "green";
+    body.style.backgroundColor = "green";
   }
 });
 
